4033361a490f465fe520c67c55227b47f074d8fe
Akash Sihag <akash.sihag@rakuten.com>
2022-03-02 01:40:57 +0800

    Setup repo and code to scan gcp resources based on labels

---
+++ deletion-script.sh
@@ -0,0 +1,10 @@
+set -x
+bq rm -f -t eventmart.dim_event_summary_v2
+bq rm -f -t eventmart.dim_im_antares_10122021
+bq rm -f -t eventmart.dim_im_human_10122021
+bq rm -f -t eventmart.dim_im_item_master_10122021
+bq rm -f -t eventmart.dim_im_item_metadata_10122021
+bq rm -f -t eventmart.dim_im_item_metadata_20211222
+bq rm -f -t eventmart.dim_im_merchant_10122021
+bq rm -f -t eventmart.dim_im_product_science_10122021
+bq rm -f -t eventmart.dim_rcp_em_order_details-20211210
